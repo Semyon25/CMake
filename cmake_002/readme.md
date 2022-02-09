8037966265564f8162581640f0e970eba0e4e50c
Semyon <semen567459@mail.ru>
2022-02-09 16:28:13 +0300

    added boost library

--- cmake_002/readme.md
+++ cmake_002/readme.md
@@ -1,1 +1,8 @@
 # Подключение библиотеки boost
+
+## Windows
+
+- На сайте https://www.boost.org/users/download/ скачать *Prebuilt windows binaries*, чтобы не компилировать boost вручную.
+- Установить boost в папку **C:\Program Files\boost**
+- В **CMakeLists.txt** указать подключаемые модули библиотеки boost.
+- Дальнейшие действия такие же, как и в первом примере.
--- cmake_002/src/main.cpp
+++ cmake_002/src/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <boost/date_time/gregorian/gregorian.hpp>
 
 using namespace std;
 
+std::string utc_date()
+{
+    namespace bg = boost::gregorian;
+
+    static char const* const fmt = "%d.%m.%Y";
+    std::ostringstream ss;
+    
+    ss.imbue(std::locale(std::cout.getloc(), new bg::date_facet(fmt)));
+    ss << bg::day_clock::universal_day();
+    return ss.str();
+}
+
 void main()
 {
-    
+    cout << "Current date: " << utc_date() << endl;
 }
